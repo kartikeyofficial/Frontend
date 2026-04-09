c6a49c39684fc3043383613d132d6a673d43977c
kartikeyofficial <kartikeygithubproject@gmail.com>
2026-04-09 21:58:01 +0530

    React

---
+++ React & Redux/first-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import {kkButton} from "./kkButton";
+function App(){
+  return <div>
+    <h1>
+    Hello World!
+  </h1>
+   <kkButton></kkButton>
+  </div>
+  
+  
+  
+  
+}
+export default App; 
---
+++ React & Redux/first-react-app/src/kkButton.jsx
@@ -0,0 +1,3 @@
+export function kkButton(){
+  return <button>Like this Video</button>
+}
