43e702a7f05ab873ea912da9ccb6314641a07190
kartikeyofficial <kartikeygithubproject@gmail.com>
2026-04-17 16:24:18 +0530

    Calculator

---
+++ React & Redux/Projects/4-calculator/src/App.jsx
@@ -0,0 +1,13 @@
+import styles from './App.module.css'
+import Display from './components/Display';
+import ButtonsContainer from './components/ButtonsContainer';
+function App(){
+  return (<center>
+  <div className={styles.calculator}>
+    <Display></Display>
+    <ButtonsContainer></ButtonsContainer>
+  </div>
+  </center>
+  );
+}
+export default App;
---
+++ React & Redux/Projects/4-calculator/src/components/Display.jsx
@@ -0,0 +1,5 @@
+import styles from './Display.module.css'
+const Display = ()=>{
+  return <input className={styles.display} type="text" name="" id="" />;
+}
+export default Display; 
