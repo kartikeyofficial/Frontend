ce3f7d245496497e2d5c49146fa15a5700e2b0f2
kartikeyofficial <kartikeygithubproject@gmail.com>
2026-04-20 17:59:54 +0530

    Small Changes

--- React & Redux/Projects/4-calculator/src/components/ButtonsContainer.jsx
+++ React & Redux/Projects/4-calculator/src/components/ButtonsContainer.jsx
@@ -1,6 +1,6 @@
 import styles from './ButtonsContainer.module.css';
 const ButtonsContainer = ()=>{
-  const buttonNames = ['C','1','2','+','3','4','5','-','6','7','8','*','9','0','/'];
+  const buttonNames = ['C','1','2','+','3','4','-','5','6','*','7','8','/','9','0'];
 
 
   return <div className={styles.buttonsContainer}>
